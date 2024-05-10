3c18f7e75db1a488900b82e9236f0327e689e40e
yusei53 <yusei530922@gmail.com>
2024-05-10 19:25:33 +0900

    ボタンの色を変えた

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Box, Button, Typography } from "@mui/material";
 export default function Home() {
   return (
     <Box>
-      <Button variant="contained" href="/yuse">
+      <Button variant="contained" color="inherit" href="/yuse">
         Contained
       </Button>
       <Typography>muiの勉強会</Typography>
