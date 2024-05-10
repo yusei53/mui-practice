e7a201bdb37d46967a79a91fcf2510a2dbf71779
yusei53 <yusei530922@gmail.com>
2024-05-10 19:40:58 +0900

    cssを書いてみた

--- app/yuse/page.tsx
+++ app/yuse/page.tsx
@@ -4,7 +4,19 @@ import React from "react";
 const page = () => {
   return (
     <Box>
-      <Typography>Hello World!</Typography>
+      <Typography
+        fontSize={"100px"}
+        color={"red"}
+        bgcolor={"black"}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        // marginTop={"100px"}
+        mt={5}
+        p={5}
+      >
+        Hello World!
+      </Typography>
     </Box>
   );
 };
